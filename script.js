56a72a8e5bd4bd00bdab87f2ed7459ddd12d8171
Florin <moisi.florin13@gmail.com>
2018-11-20 17:49:30 +0200

    Final Version

--- script.js
+++ script.js
@@ -1,22 +1,8 @@
-/*
-$(document).ready(function() {
-    $('#device, #prices').hide();
-
-    $(".meniu li").each(function(i) {
-        $(this).click(function() {
-            $(".meniu-wrapper").find("div:eq('" + i + "')").show().siblings().hide();
-        });
-    });
-});
-
-*/
-
 var divs = ["Menu1", "Menu2", "Menu3"];
 var visibleDivId = null;
 
 function toggleVisibility(divId) {
-    if (visibleDivId === divId) {
-    } else {
+    if (visibleDivId === divId) {} else {
         visibleDivId = divId;
     }
     hideNonVisibleDivs();
@@ -35,18 +21,8 @@ function hideNonVisibleDivs() {
     }
 }
 
-/*
-function showMenu(){
-    var x = document.getElementById('device');
-    if(x.style.display === "none") {
-        x.style.display = "block";
-    }else {
-        x.style.display = "none"
-    }
-}
-*/
-
-$('.meniu li').on('click', function(e){
-        $('.meniu li').removeClass('active');
-        $(e.currentTarget).addClass('active');
-    });
+$('.menu li').on('click', function (e) {
+    $('.menu li').removeClass('active');
+    $(e.currentTarget).addClass('active');
+    $(e.currentTarget).removeClass('menu-tab:hover');
+});
